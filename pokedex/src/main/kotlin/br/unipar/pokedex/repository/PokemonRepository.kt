b7fb834fe4b12fc732762a0039925fdddecdd731
iLeonard0 <folleleonardoprogramming@gmail.com>
2025-04-23 20:39:13 -0300

    creating repository and save function

--- pokedex/src/main/kotlin/br/unipar/pokedex/repository/PokemonRepository.kt
+++ pokedex/src/main/kotlin/br/unipar/pokedex/repository/PokemonRepository.kt
@@ -1,8 +1,20 @@
 package br.unipar.pokedex.repository
 
+import br.unipar.pokedex.model.Pokemon
+import com.google.cloud.firestore.Firestore
+import com.google.firebase.cloud.FirestoreClient
 import org.springframework.stereotype.Repository
 
 @Repository
 class PokemonRepository {
-    private val firestore : String = ""
+    private val firestore: Firestore = FirestoreClient.getFirestore()
+    private val collection = firestore.collection("pokedex")
+
+    fun save(pokemon: Pokemon) : Pokemon {
+        val document = collection.document(pokemon.numeroPokedex!!)
+        val registeredPokemon = pokemon.copy(numeroPokedex = document.id)
+        document.set(registeredPokemon)
+
+        return registeredPokemon
+    }
 }
